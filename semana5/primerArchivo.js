35d5433f3ba01ae04a9b87e62f04e2457d275013
guillermosifu <sifuentes1201@gmail.com>
2022-08-03 23:03:52 -0500

    new file

---
+++ semana5/primerArchivo.js
@@ -0,0 +1,14 @@
+const numero1 = 10;
+const numero2 = 20;
+
+
+
+resultado = numero1 + numero2;
+producto = numero1 * numero2;
+
+console.log(resultado)
+console.log(producto)
+
+//recuerden 
+//alert : es una alerta del navegador 
+// CONSOLE.LOG = imprimir en consola 
