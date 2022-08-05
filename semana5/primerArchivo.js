5c5f7ac9fa913d5189a6faaf5a3ab4275c30747a
guillermosifu <sifuentes1201@gmail.com>
2022-08-04 20:29:24 -0500

    variables

--- semana5/primerArchivo.js
+++ semana5/primerArchivo.js
@@ -1,6 +1,8 @@
 const numero1 = 10;
 const numero2 = 20;
 
+
+
 // recuerden el ; no importa
 let resultado = 0;
 
@@ -25,3 +27,63 @@ console.error("Suma Error:", resultado);
 
 resultado = numero1 - numero2;
 console.warn("Resultado: ", resultado);
+
+//vamos hacer una calculadora
+//prompt = es una ventana emergente que nos pide un valor 
+//esto abre una ventana tipo alert! 
+//el valor que ingresamos se guarda en una variable 
+
+
+//necesito que el usuario ingrese el tipo de operacion
+
+let valor1 = //prompt("ingresa valor 1");
+let valor2 = // prompt("ingresa valor 2");
+
+const operacion = //prompt("ingrese su operacion que puede se + - * /");
+
+let resultadoCalculadora= 0;
+
+//para poder hacer calculos debo convertir mi variable de string a number 
+//parseInt : convertir un string en number 
+//+ = delante de la variable y esta se convierte en number ;
+
+if(operacion ==="+"){
+    resultadoCalculadora = +valor1 + +valor2;
+    console.log("resultado",resultadoCalculadora);
+}else if(operacion ==="-"){
+    resultadoCalculadora= +valor1 -+valor2
+    console.log("resultado",resultadoCalculadora)
+}else if(operacion=== "*"){
+    resultadoCalculadora= +valor1 * +valor2;
+    console.log("la multiplicacion es :" , resultadoCalculadora)
+}
+// hay un paso extra para la division 
+else if(operacion === "/"){
+    if(+valor2 !== 0){
+        resultadoCalculadora = +valor1 / +valor2;
+        console.log("es el resultado" , resultadoCalculadora)
+    } else{
+        console.warn("el divisor deber ser distinto a 0")
+    }
+} else{
+    console.error("operacion no valida")
+}
+
+
+//haremos un algoritmo para poder detectar si un numero es par o impar
+
+const numeroMultiplo = 12;
+
+//la forma en la que se puede saber si un numero es es usaso %
+// porque es una operacion que nos da un residuo
+
+//mod = %
+
+if (numeroMultiplo % 2 === 0){
+    console.log("numero par", numeroMultiplo);
+}
+if(numeroMultiplo % 3 === 0){
+    console.log("multiplo de 3", numeroMultiplo)
+}
+
+console.log()
