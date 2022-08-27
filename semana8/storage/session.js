41fc2a6ea1dc8a63d49eeb2c3dd1f9012fdbaa4e
guillermosifu <sifuentes1201@gmail.com>
2022-08-26 23:04:03 -0500

    session

---
+++ semana8/storage/session.js
@@ -0,0 +1,24 @@
+const crear = document.querySelector("#crear-session")
+const obtener = document.querySelector("#obtener-session")
+const eliminar = document.querySelector("#eliminar-session")
+
+
+crear.onclick = function(){
+    //para poder crea una variable em sessiom sotrage invocamoa session
+
+    sessionStorage.setItem(
+        "variable_1",
+        "mi primera variable en session storage"
+    );
+};
+
+obtener.onclick = function () {
+    const variable = sessionStorage.getItem("variable_1");
+    console.log(variable);
+
+}
+
+eliminar.onclick = function (){
+    sessionStorage.removeItem("variable_1")
+}
+
