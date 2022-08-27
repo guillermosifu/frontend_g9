d988a6c0a6585ed1f540f8aafd0a427a8a08a63e
guillermosifu <sifuentes1201@gmail.com>
2022-08-27 10:44:14 -0500

    prueba2

--- semana8/events/operaciones.js
+++ semana8/events/operaciones.js
@@ -1,30 +1,32 @@
 function validarSiEsNumero(numero, tipo) {
-    return isNaN(numero) ? `mensaje Error : no se puede ${tipo}`: numero;
-}
-
-function sumar (numero1 , numero2){
-
-const suma = +numero1 + +numero2 ;
-
-return validarSiEsNumero(suma,"sumar")
-
-}
-
-function restar (numero1 , numero2){
-    const calcResta = +numero1 - +numero2 ;
-    return validarSiEsNumero(calcResta ,"restar")
-}
-
-
-function multiplicar(numero1, numero2){
-    const calcMultiplicar= +numero1 * +numero2;
-    return validarSiEsNumero(calcMultiplicar, "multiplicar")
-}
-function dividir(numero1, numero2){
-if(+numero2 === 0){
-    return "Error no se puede divir entre 0"
-}
-    
-    const calcDividir= +numero1 / +numero2;
-    return validarSiEsNumero(calcDividir, "dividir")
-}
+    return isNaN(numero) ? `Mensaje Error: no se puede ${tipo}` : numero;
+  }
+  
+  function sumar(numero1, numero2) {
+    const suma = +numero1 + +numero2;
+  
+    return validarSiEsNumero(suma, "sumar");
+  }
+  
+  function restar(numero1, numero2) {
+    const calcResta = +numero1 - +numero2;
+  
+    return validarSiEsNumero(calcResta, "restar");
+  }
+  
+  function multiplicar(numero1, numero2) {
+    const calcMultiplicacion = +numero1 * +numero2;
+  
+    return validarSiEsNumero(calcMultiplicacion, "multiplicar");
+  }
+  
+  function dividir(numero1, numero2) {
+    if (+numero2 === 0) {
+      // termina la ejeccion de la funcion y retorn ese string
+      return "Error: no se puede dividir por 0";
+    }
+  
+    const calcDivision = +numero1 / +numero2;
+    return validarSiEsNumero(calcDivision, "dividir");
+  }
+  
